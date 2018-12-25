8fdf3215dbf27e26d5856bf8d86cf7bd029fd898
Marcelo Kath <marcelokathmk@gmail.com>
2018-12-25 12:01:35 -0200

    Commit inicial do projeto backend.

---
+++ src/main/java/br/com/softplan/processManagementBackEnd/ProcessManagementBackEndApplication.java
@@ -0,0 +1,14 @@
+package br.com.softplan.processManagementBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessManagementBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessManagementBackEndApplication.class, args);
+	}
+
+}
+
